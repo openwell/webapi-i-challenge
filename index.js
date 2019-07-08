9484839183874a3c59886a29a5de21dc129e8bdf
openwell <timileyinojo@yahoo.com>
2019-07-08 14:09:15 +0100

    added route to create a user

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
-
+const db = require("./data/db");
 const server = express();
 
-server.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Post End Point....Creation of User
+server.post("/api/users", (req, res) => {
+  db.insert(req.body)
+    .then(data => {
+      res.status(200).json({
+        data: data
+      });
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
 server.listen(4000, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n");
