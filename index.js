dcccdc819adeb3a418bef3f13329473efa416070
openwell <timileyinojo@yahoo.com>
2019-07-08 13:17:11 +0100

    setup of server and installation of packages

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// implement your API here
+const express = require("express");
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+server.listen(4000, () => {
+  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+});
