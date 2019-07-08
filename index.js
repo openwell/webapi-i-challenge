293c6af5125dfd7e6cee7ad0e489e62ad390c8f4
openwell <timileyinojo@yahoo.com>
2019-07-08 14:25:31 +0100

    added route to fetch a user by id

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.get("/", (req, res) => {
 server.post("/api/users", (req, res) => {
   db.insert(req.body)
     .then(data => {
-      res.status(200).json({
+      res.status(201).json({
         data: data
       });
     })
@@ -22,16 +22,29 @@ server.post("/api/users", (req, res) => {
 });
 // GET  ENd Point .... Fetching of All Users
 server.get("/api/users", (req, res) => {
-    db.find()
-      .then(data => {
-        res.status(200).json({
-          data: data
-        });
-      })
-      .catch(err => {
-        res.send(err);
+  db.find()
+    .then(data => {
+      res.status(200).json({
+        data: data
       });
-  });
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+// GET  ENd Point .... Fetching a User by Id
+server.get("/api/users/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then(data => {
+      res.status(200).json({
+        data: data
+      });
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
 server.listen(4000, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n");
